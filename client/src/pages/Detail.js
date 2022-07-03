82da461c7c62090f01b82ec7a3f26cc1ddb64a99
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-07-02 19:39:10 -0500

    added cart sumation

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
@@ -26,6 +31,13 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
   
   return (
     <>
@@ -39,7 +51,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
