6077bda9ec1423c49c0a0e5dedb213b77ca8c505
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-07-04 20:20:35 -0500

    implemented stripe

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Success from './pages/Success';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
@@ -67,6 +68,10 @@ function App() {
               <Route 
                 path="*" 
                 element={<NoMatch />}
+              />
+              <Route
+                path="/success"
+                element={<Success />}
               />
             </Routes>
           </StoreProvider>
